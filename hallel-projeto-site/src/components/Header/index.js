e581920e5d50e768b12b438d09ff0035732652ce
nythw <sthefanydrawings@gmail.com>
2023-01-13 14:57:59 -0400

    homepage-mobile

--- hallel-projeto-site/src/components/Header/index.js
+++ hallel-projeto-site/src/components/Header/index.js
@@ -1,34 +1,48 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './style.css';
 import Logo from '../../images/LogoHallel.png';
 
 
-function Header(){
-  
-  return(
-    
-    <nav className = "topbar">
-    
-    <div>
-    
-      <img id = "logo" src={Logo} alt = "logo"/>
-      <Link to = "/">Início</Link>
-    
-      
-     <Link to = "/">Igreja</Link>
-      
-      <Link to = "/">Ministérios</Link>
-      <Link to = "/">Agenda</Link>
-      <Link to = "/">Loja</Link>
-      <Link to = "/">Doações</Link>
-      <Link to = "/">Contato</Link>
-     
-    <button id = "btn">Entre</button>
-    
-    </div> 
+function Header() {
+
+  return (
+
+    <nav className="topbar">
+
+      <div class="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label class="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
+
+        <ul class="menu__box">
+          <li><a class="menu__item" href="#">Inicio</a></li>
+          <li><a class="menu__item" href="#">Igreja</a></li>
+          <li><a class="menu__item" href="#">Ministerios</a></li>
+          <li><a class="menu__item" href="#">Agenda</a></li>
+          <li><a class="menu__item" href="#">Loja</a></li>
+          <li><a class="menu__item" href="#">Doacoes</a></li>
+          <li><a class="menu__item" href="#">Contato</a></li>
+        </ul>
+      </div>
+      <img id="logo" src={Logo} alt="logo" />
+      <div id="space"></div>
+
+      <div id="topicos">
+        <Link to="/">Início</Link>
+        <Link to="/">Igreja</Link>
+        <Link to="/">Ministérios</Link>
+        <Link to="/">Agenda</Link>
+        <Link to="/">Loja</Link>
+        <Link to="/">Doações</Link>
+        <Link to="/">Contato</Link>
+      </div>
+
+      <button id="btn">Entre</button>
+
     </nav>
-    )
-  
+  )
+
 }
 
 export default Header;
