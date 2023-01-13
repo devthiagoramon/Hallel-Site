f03211ee60d8627f3adbfc67ba30a688a374ce04
nythw <sthefanydrawings@gmail.com>
2023-01-13 18:03:11 -0400

    tome papai

--- hallel-projeto-site/src/components/Slide/index.js
+++ hallel-projeto-site/src/components/Slide/index.js
@@ -5,91 +5,98 @@ import Img3 from '../../images/slide3.jpeg';
 import Img4 from '../../images/slide4.jpeg';
 import Img5 from '../../images/slide5.jpeg';
 
+import React, { Component } from 'react'
+
 //  images controls
-var time = 0
-var slidePosition = 0
+class Slide extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      slides: document.getElementsByClassName("slide"),
+      slidePosition: 0,
+      time: 0,
+    }
+    this.autoShow = this.autoShow.bind(this)
+    this.SlideShow = this.SlideShow.bind(this)
+    this.currentSlide = this.currentSlide.bind(this)
+  }
 
-function autoShow() {
-  var i;
-  var slides = document.getElementsByClassName("slide");
+  componentDidMount() {
+    this.autoShow()
+  }
 
-  for (i = 0; i < slides.length; i++)
-    slides[i].style.display = "none";
+  autoShow() {
+    for (let i = 0; i < this.state.slides.length; i++)
+      this.state.slides[i].style.display = "none";
 
-  slidePosition++;
+    this.state.slidePosition++;
 
-  if (slidePosition > slides.length)
-    slidePosition = 1;
+    if (this.state.slidePosition > this.state.slides.length)
+      this.state.slidePosition = 1;
 
-  slides[slidePosition - 1].style.display = "block";
+    this.state.slides[this.state.slidePosition - 1].style.display = "block";
 
-  time = setTimeout(autoShow, 10000)
+    this.state.time = setTimeout(this.autoShow, 10000)
+  }
+  SlideShow(n) {
+
+    if (n > this.state.slides.length)
+      this.state.slidePosition = 1;
+
+    if (n < 1)
+      this.state.slidePosition = this.state.slides.length;
+
+    for (let i = 0; i < this.state.slides.length; i++)
+      this.state.slides[i].style.display = "none";
+
+    this.state.slides[this.state.slidePosition - 1].style.display = "block"
+  }
+  currentSlide(n) {
+    this.SlideShow(this.state.slidePosition = n);
+    clearTimeout(this.state.time)
+    this.autoShow()
+  }
+  render() {
+
+    return (
+      <div className="slider fadee">
+        <input type="radio" id="r1" />
+        <input type="radio" id="r2" />
+        <input type="radio" id="r3" />
+        <input type="radio" id="r4" />
+        <input type="radio" id="r5" />
+
+        <div className="slide">
+          <img src={Img1} alt="Img 1" />
+        </div>
+
+        <div className="slide">
+          <img src={Img2} alt="Img 2" />
+        </div>
+
+        <div className="slide">
+          <img src={Img3} alt="Img 3" />
+        </div>
+
+        <div className="slide">
+          <img src={Img4} alt="Img 4" />
+        </div>
+
+        <div className="slide">
+          <img src={Img5} alt="Img 5" />
+        </div>
+
+        <div className="navigation-manual">
+          <label htmlFor="r1" className="manual-btn" onClick={() => this.currentSlide(0)} ></label>
+          <label htmlFor="r2" className="manual-btn" onClick={() => this.currentSlide(1)} ></label>
+          <label htmlFor="r3" className="manual-btn" onClick={() => this.currentSlide(2)} ></label>
+          <label htmlFor="r4" className="manual-btn" onClick={() => this.currentSlide(3)} ></label>
+          <label htmlFor="r5" className="manual-btn" onClick={() => this.currentSlide(4)} ></label>
+        </div>
+
+      </div>
+    )
+  }
 }
-
-autoShow()
-
-function SlideShow(n) {
-  var slides = document.getElementsByClassName("slide");
-
-  if (n > slides.length)
-    slidePosition = 1;
-
-  if (n < 1)
-    slidePosition = slides.length;
-
-  for (var i = 0; i < slides.length; i++)
-    slides[i].style.display = "none";
-
-  slides[slidePosition - 1].style.display = "block"
-}
-
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-  clearTimeout(time)
-  autoShow()
-}
-
-function Slide() {
-  return (
-    <div className="slider fadee">
-      <input type="radio" id="r1" />
-      <input type="radio" id="r2" />
-      <input type="radio" id="r3" />
-      <input type="radio" id="r4" />
-      <input type="radio" id="r5" />
-
-      <div className="slide">
-        <img src={Img1} alt="Img 1" />
-      </div>
-
-      <div className="slide">
-        <img src={Img2} alt="Img 2" />
-      </div>
-
-      <div className="slide">
-        <img src={Img3} alt="Img 3" />
-      </div>
-
-      <div className="slide">
-        <img src={Img4} alt="Img 4" />
-      </div>
-
-      <div className="slide">
-        <img src={Img5} alt="Img 5" />
-      </div>
-
-      <div className="navigation-manual">
-        <label htmlFor="r1" className="manual-btn" onClick={() => currentSlide(0)} ></label>
-        <label htmlFor="r2" className="manual-btn" onClick={() => currentSlide(1)} ></label>
-        <label htmlFor="r3" className="manual-btn" onClick={() => currentSlide(2)} ></label>
-        <label htmlFor="r4" className="manual-btn" onClick={() => currentSlide(3)} ></label>
-        <label htmlFor="r5" className="manual-btn" onClick={() => currentSlide(4)} ></label>
-      </div>
-
-    </div>
-  );
-}
-
-
 
 export default Slide;
