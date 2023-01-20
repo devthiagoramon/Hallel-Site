bfc156d5f91371b4ac04dd67439c98ed9ce72501
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2023-01-20 12:33:49 -0400

    update App.js
    Atualização para o footer ficar fixo no bottom da página

--- hallel-projeto-site/src/App.js
+++ hallel-projeto-site/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import Footer from "./components/Footer";
 import RoutesApp from "./routes";
 
 function App() {
   return (
-      <RoutesApp />
+      <div className = "page-container">
+        
+          <div className = "content-wrap">
+            <RoutesApp />
+          </div>
+        <Footer />
+      </div>
   );
 }
 
 export default App;
+
