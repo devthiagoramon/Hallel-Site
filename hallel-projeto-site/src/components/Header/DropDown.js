05404ca31ba4aad4adaf1f1e274b461b4fc7069d
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2023-02-12 17:38:51 -0400

    create dropdown.js
    opções do seletor "igreja" .

---
+++ hallel-projeto-site/src/components/Header/DropDown.js
@@ -0,0 +1,33 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Link } from "react-router-dom";
+import './dropDown.css';
+
+
+function DropDown() {
+  return (
+    
+    <div className= "Dropdown">
+    <Dropdown>
+      <Dropdown.Toggle variant="sucess btn-lg" id="dropdown-basic">
+        Igreja
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu id = "menu">
+        <Dropdown.Item>
+<Link to="/sobre">Quem somos</Link>
+        </Dropdown.Item>
+        
+          <div className="dropdown-divider"></div>
+        
+        <Dropdown.Item>
+<Link to="/fundadora">Fundadora</Link>
+        </Dropdown.Item>
+        
+         <div className="dropdown-divider"></div>
+      </Dropdown.Menu>
+    </Dropdown>
+    
+    </div>
+  )
+}
+export default DropDown;
