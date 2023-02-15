2cc9f35a69e37089b19dfe800154b48493ff5056
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2023-02-15 16:53:39 -0300

    add arquivos (pedido oração)

---
+++ Cards/pedido_oracao/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './style.css';
+
+    function pedido_oracao(){
+
+      return(
+
+        <Card className = "card mx-auto w-40" style = {{width: '18rem', border: '2px solid #D94814'}}>
+
+
+            <Card.Body>
+
+                <Card.Title>Pedido de oração</Card.Title>
+
+                    <div className = "space-in">
+
+                        <img id = "imagem" src = "https://www.basilicacoracaodemaria.com/area/img/noticias/6f7757f890370ad6cd8c9d062b5bac4c.jpg" alt = "Pedido_img"/>
+
+                            <Card.Text>
+                                Resgatando para através do louvor, da formação e do amor por excelência.
+                            </Card.Text>
+                    </div>
+
+                <Button id = "bt" variant = "primary">Enviar</Button>
+                
+            </Card.Body>
+        </Card>
+      );
+    }
+
+    export default pedido_oracao;
