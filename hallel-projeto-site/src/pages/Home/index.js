1bbc1dc936352725a06f270c398aabc5c6378b0e
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2023-02-24 13:49:42 -0300

    Update index.js

--- hallel-projeto-site/src/pages/Home/index.js
+++ hallel-projeto-site/src/pages/Home/index.js
@@ -1,7 +1,5 @@
 import { Link } from "react-router-dom";
 import Slide from "../../components/Slide";
-import Fundadora from "../Fundadora";
-import Sobre from "../Sobre";
 import Card_oracao from "./Cards/pedido_oracao";
 
 function Home() {
@@ -9,10 +7,10 @@ function Home() {
     <div>
       <Slide />
 
-      <Fundadora />
+
       
       <br />
-      <Sobre />
+   
       <br />
       <Link to="/administrador/eventos">ADMINISTRADOR: Eventos</Link>
       <br />
