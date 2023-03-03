72027f3ed527b2e5109f930720f5fd56f703100b
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2023-03-03 08:56:26 -0300

    Update index.js

--- hallel-projeto-site/src/pages/Home/Cards/pedido_oracao/index.js
+++ hallel-projeto-site/src/pages/Home/Cards/pedido_oracao/index.js
@@ -10,30 +10,30 @@ function pedido_oracao() {
       className="card mx-auto w-40"
       style={{ width: "30rem", border: "2px solid #D94814" }}
     >
-      <Card.Body>
-        <Card.Title>Pedido de oração</Card.Title>
+      <Card.Body id = "corpo">
+        <Card.Title>
+          <h1>Pedido de oração</h1>
+          </Card.Title>
 
-        <div className="space-in" style={{display:"flex", flexDirection:"column"}}>
+        <div className="space-in" style={{display:"flex"}}>
           <img
             id="imagem"
             src="https://www.basilicacoracaodemaria.com/area/img/noticias/6f7757f890370ad6cd8c9d062b5bac4c.jpg"
             alt="Pedido_img"
           />
 
-          <Card.Text style={{width: "100%",
+          <Card.Text id = "texto" style={{width: "100%",
                             marginLeft:"0",wordBreak: "normal"}}> 
               Resgatando para através do louvor, da formação e do amor por
               excelência.
           </Card.Text>
         </div>
 
-        <Button id="bt" variant="primary">
-          {" "}
-          <Link to="/pedido">Entrar</Link>{" "}
+        <Button id="bt">
+          <Link to="/pedido">Entrar</Link>
         </Button>
+
       </Card.Body>
     </Card>
   );
 }
-
-export default pedido_oracao;
