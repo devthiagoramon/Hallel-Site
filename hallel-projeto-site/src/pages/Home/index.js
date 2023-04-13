4c6aeec80708c88b13ca04e4588bd1b0b84d03e4
Manfred Veiga <2021321227@ifam.edu.br>
2023-04-13 14:37:48 -0300

    Atualização da HomePage

--- hallel-projeto-site/src/pages/Home/index.js
+++ hallel-projeto-site/src/pages/Home/index.js
@@ -1,27 +1,34 @@
 import { Link } from "react-router-dom";
-import Slide from "../../components/Slide";
 import Card_oracao from "./Cards/pedido_oracao";
-import DoarPanel from "../../components/Panels/PanelDoacao"
-import Programacao from "../../components/SlideProg"
-import Eventos from "../../components/SlideEventos"
+import DoarPanel from "../../components/Panels/PanelDoacao";
+import Programacao from "../../components/SlideProg";
+import Eventos from "../../components/SlideEventos";
+import './homepage.css';
 
 function Home() {
+
   return (
-    <div>
-      <Slide />
+
+    <div className="homepage">
+
+      <div className="containerCapa">
+        <section className="sectionCapa">
+    <label>Seja bem-vindo(a) à Comunidade Hallel</label>
+        </section>
+      </div>
   
       <br />
-        <Link to="/administrador/eventos">ADMINISTRADOR: Eventos</Link>
+          <Link to="/administrador/eventos">ADMINISTRADOR: Eventos</Link>
       <br />
-        <Link to="/administrador/membros">ADMINISTRADOR: Membros</Link>
+          <Link to="/administrador/membros">ADMINISTRADOR: Membros</Link>
       <br />
-        <Link to="/administrador/painelFinanceiro">ADMINISTRADOR: Painel Financeiro</Link>
-        <Eventos/>
-        <Programacao/>
-        <DoarPanel/>
-        <Card_oracao/>
-
+          <Link to="/administrador/painelFinanceiro">ADMINISTRADOR: Painel Financeiro</Link>
+          <Eventos/>
+          <Programacao/>
+          <DoarPanel/>
+          <Card_oracao/>
     </div>
   );
 }
+
 export default Home;
