c579a97b2e87916050a591f7e962f0c8624eff53
FELIPE-SOARES-VALENTE <2021334440@ifam.edu.br>
2023-04-28 12:48:18 -0400

    Commit Sprint 28/04

--- hallel-projeto-site/src/components/SlideEventos/index.js
+++ hallel-projeto-site/src/components/SlideEventos/index.js
@@ -8,8 +8,9 @@ import Img7 from "../../images/AcampamentoHomem.jpg";
 import "./styleEventos.css";
 import Card from "react-bootstrap/Card";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { LinearProgress, Skeleton } from "@mui/material";
 
 //  images controls
 const SlideEventos = (props) => {
@@ -17,57 +18,80 @@ const SlideEventos = (props) => {
 
   const carrosel = useRef();
   const [width, setWidht] = useState(0);
+  const [eventos, setEventos] = useState([]);
 
   useEffect(() => {
     setWidht(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth);
   }, []);
 
+  const renderizarEventos = useMemo(() => {
+    let url = "http://localhost:8080/api/eventos";
+
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", localStorage.getItem("token"));
+
+    fetch(url, {
+      headers: myHeaders,
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((evento) => {
+        console.log(evento);
+        setEventos(evento);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <div className="body-eventos">
       <h1 id="title">Eventos</h1>
       <div className="containerCarroseulEvent">
-        <motion.div
-          ref={carrosel}
-          className="carrousel"
-          initial={{ x: 100 }}
-          animate={{ x: 0 }}
-          transition={{ duration: 0.8 }}
-          whileTap={{ cursor: "grabbing" }}
-        >
+        {eventos.length === 0 ? <div className="loaderEventoCarroseul"><Skeleton width={400} height={500} /> <Skeleton width={400} height={500} /> <Skeleton width={400} height={500} /> <Skeleton width={400} height={500} /> <Skeleton width={400} height={500} /></div> :
           <motion.div
-            className="inner"
-            drag="x"
-            dragConstraints={{ right: 0, left: -width }}
+            ref={carrosel}
+            className="carrousel"
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.8 }}
+            whileTap={{ cursor: "grabbing" }}
           >
-            {images.map((imagem2) => {
-              return (
-                <motion.div
-                  className="imagem2"
-                  key={imagem2}
-                  whileHover={{ scale: "1.02" }}
-                >
-                  <Card style={{ width: "22rem" }}>
-                    <Card.Img variant="top" src={imagem2} />
+            <motion.div
+              className="inner"
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+            >
+              {eventos.map((evento) => {
+                return (
+                  <motion.div
+                    className="imagem2"
+                    key={evento.titulo}
+                    whileHover={{ scale: "1.02" }}
+                  >
+                    <Card style={{ width: "22rem" }}>
+                      <Card.Img variant="top" src={evento.imagem} />
 
-                    <Card.Body>
-                      <Card.Title>Título do Evento</Card.Title>
-                      <Card.Text>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Nunc sit amet sem ac diam elementum suscipit vel
-                          eget dui. In metus est, lobortis at fringilla ac,
-                          euismod at metus.
-                        </p>
-                        <time>??/??/????</time> <br />
-                        <time>00:00</time>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                </motion.div>
-              );
-            })}
-          </motion.div>
-        </motion.div>
+                      <Card.Body>
+                        <Card.Title>{evento.titulo}</Card.Title>
+                        <Card.Text>
+                          <p>
+                            {evento.descricao}
+                          </p>
+                          <time>{evento.date}</time> <br />
+                          <time>{evento.horario}</time>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          </motion.div>}
       </div>
     </div>
   );
