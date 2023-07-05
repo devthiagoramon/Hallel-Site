ab1cdc88906c9258ef749abf503aaa70854e003f
devthiagoramon <dev.thiagoramon@gmail.com>
2023-07-05 12:45:26 -0400

    editar modal de despesas

--- hallel-projeto-site/src/pages/Administrador/Eventos/despesa_evento/DespesaEvento.js
+++ hallel-projeto-site/src/pages/Administrador/Eventos/despesa_evento/DespesaEvento.js
@@ -3,14 +3,19 @@ import { useParams } from 'react-router-dom'
 import "./despesa_evento.css"
 import HeaderDespesasEventos from './HeaderDespesasEventos';
 import axios from 'axios';
-import { CircularProgress } from '@mui/material';
+import { Alert, CircularProgress, Snackbar } from '@mui/material';
 import BodyDespesaEvento from './BodyDespesaEvento';
+import ModalEditDespesaEvento from './ModalEditDespesaEvento';
 
 
 const DespesaEvento = () => {
 
     const { idEvento } = useParams();
     const [evento, setEvento] = useState(null);
+    const [openModal, setOpenModal] = useState(true);
+    const [despesaSelected, setDespesaSelected] = useState(null);
+    const [editPopUp, setEditPopUp] = useState(null);
+    const [openPopUp, setopenPopUp] = useState(false);
 
     useEffect(() => {
         axios.get("http://localhost:8080/api/administrador/evento/" + idEvento + "/list", {
@@ -25,12 +30,32 @@ const DespesaEvento = () => {
         })
     }, [])
 
+    const handleClosePopUp = () => {
+        setEditPopUp(null);
+        setopenPopUp(false);
+    }
+
     return (
         <div className='container_despesas_eventos'>
             {evento !== null ?
                 <>
                     <HeaderDespesasEventos evento={evento} />
-                    <BodyDespesaEvento evento={evento}/>
+                    <BodyDespesaEvento evento={evento} />
+                    <ModalEditDespesaEvento openModal={openModal} setOpenModal={setOpenModal} />
+                    {editPopUp !== null &&
+                        <>
+                            {editPopUp === true &&
+                                <Snackbar open={openPopUp} onClose={handleClosePopUp} autoHideDuration={3000} >
+                                    <Alert severity='success' sx={{width: "100%"}}>Editado com sucesso</Alert>
+                                </Snackbar>
+                            }
+                            {editPopUp === false &&
+                                <Snackbar open={openPopUp} onClose={handleClosePopUp} autoHideDuration={3000} >
+                                    <Alert severity='success' sx={{width: "100%"}}>Error na edição</Alert>
+                                </Snackbar>
+                            }
+                        </>
+                    }
                 </> :
                 <>
                     <div className='container_load_despesas_eventos'>
---
+++ hallel-projeto-site/src/pages/Administrador/Eventos/despesa_evento/ModalEditDespesaEvento.js
@@ -0,0 +1,129 @@
+import { AddRounded, DoneRounded, Edit, ErrorRounded } from '@mui/icons-material';
+import { Textarea } from '@mui/joy';
+import { Box, Button, CircularProgress, MenuItem, Modal, Select, TextField } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const ModalEditDespesaEvento = (props) => {
+
+    const { idEvento, despesaSelected, openModal, setOpenModal, setEditarPopUp, setOpenPopUp } = props;
+
+    const [despesaEvento, setDespesaEvento] = useState(despesaSelected);
+
+    const [enviando, setEnviando] = useState(false);
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
+    const styleInnerModal = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "#F2F2F8",
+        borderRadius: "12px",
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const editarDespesaEvento = () => {
+
+        setEnviando(true);
+        axios.put("http://localhost:8080/api/administrador/eventos/" + idEvento + "/despesa/" + despesaEvento.id + "/edit", {
+            ...despesaEvento
+        }, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        }).then(() => {
+            setEditarPopUp(true);
+            setOpenModal(false);
+            setOpenPopUp(true);
+            setEnviando(false);
+        }).catch(() => {
+            setEditarPopUp(false);
+            setOpenPopUp(true);
+            setEnviando(false);
+        })
+    }
+
+    const handleChangeSelect = (e) => {
+        setDespesaEvento(prevState => {
+            return { ...prevState, num_tipoDespesa: e.target.value }
+        })
+    }
+
+    return (
+        <>
+            {/*
+            <Modal open={openModal} onClose={handleCloseModal} >
+                <Box sx={styleInnerModal}>
+                    <div className='head_edit_despesas_evento'>
+                        <label>Editando Despesas</label>
+                    </div>
+                    <div className='container_inputs_edit_despesas_evento'>
+                        <div className='inputs_edit_despesas_evento'>
+                            <label>Nome da despesa <span className='obrigatorio'>*</span></label>
+                            <TextField size='small' value={despesaEvento.nome} onChange={(e) => {
+                                setDespesaEvento(prevState => {
+                                    return { ...prevState, nome: e.target.value }
+                                })
+                            }} />
+                            <label>Descrição <span className='obrigatorio'>*</span></label>
+                            <Textarea variant='outlined' value={despesaEvento.descricao} maxRows={5} onChange={(e) => {
+                                setDespesaEvento(prevState => {
+                                    return { ...prevState, descricao: e.target.value }
+                                })
+                            }} />
+                            <label>Tipo da despesa <span className='obrigatorio'>*</span></label>
+                            <Select size='small' value={despesaEvento.num_tipoDespesa} onChange={handleChangeSelect}>
+                                <MenuItem value={1}>Dinheiro</MenuItem>
+                                <MenuItem value={2}>Alimento</MenuItem>
+                                <MenuItem value={3}>Limpeza</MenuItem>
+                                <MenuItem value={4}>Acessório</MenuItem>
+                                <MenuItem value={5}>Roupas</MenuItem>
+                            </Select>
+                            {despesaEvento.num_tipoDespesa !== '' &&
+                                <>
+                                    {despesaEvento.num_tipoDespesa !== 1
+                                        ?
+                                        <>
+                                            <label>Quantidade</label>
+                                            <TextField value={despesaEvento.quantidade}
+                                                onChange={(e) => {
+                                                    setDespesaEvento(prevState => {
+                                                        return { ...prevState, quantidade: e.target.value }
+                                                    })
+                                                }} type='number' size='small' sx={{ maxWidth: "30%" }} />
+                                        </>
+                                        :
+                                        <>
+                                            <label>Valor</label>
+                                            <TextField value={despesaEvento.valor}
+                                                onChange={(e) => {
+                                                    setDespesaEvento(prevState => {
+                                                        return { ...prevState, valor: e.target.value }
+                                                    })
+                                                }} size='small' type='number' sx={{ maxWidth: "30%" }} />
+                                        </>}
+                                </>
+                            }
+                        </div>
+                    </div>
+                    <div className='container_btn_edit_despesas_evento'>
+                        <>
+                            {enviando === false ?
+                                <Button variant='contained' endIcon={<Edit />} onClick={editarDespesaEvento}>Adicionar</Button> :
+                                <Button variant='contained'>Enviando <CircularProgress sx={{ color: "#FAF4F4" }} /></Button>}
+                        </>
+                    </div>
+                </Box>
+            </Modal>
+                                */}
+        </>
+    )
+}
+
+export default ModalEditDespesaEvento
