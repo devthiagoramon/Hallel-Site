d63bc2b181b6364d23d65498d2898091ea234e20
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-07-12 13:52:13 -0400

    tela de informações individuais do assosciado(adm)

--- hallel-projeto-site/src/pages/Administrador/Financeiro/associados/HistoricoAssociado/index.js
+++ hallel-projeto-site/src/pages/Administrador/Financeiro/associados/HistoricoAssociado/index.js
@@ -1,14 +1,51 @@
 
-const HistoricoAssociado = () =>{
+import "./styleHistoricoAssociado.css";
 
-    return(
 
-        <div>
-
-            <h3>Historico Associado</h3>
-        
+const Parte2 = () => {
+    const op = [
+        'Anual',
+        'Mensal',
+        'Bimestral',
+        'Trimestral',
+        'Semestral'
+    ];
+    return (
+        <div className="infos">
+            <div className="infosAs">
+                <h3>Nome: teste</h3>
+                <h3>Cpf: teste</h3>
+                <h3>Idade: teste</h3>
+            </div>
+            <div className="filtros">
+                <select >
+                    {op.map((option, index) => (
+                        <option key={index}>{option}</option>
+                    ))}
+                </select>
+            </div>
         </div>
     );
 }
 
-export default  HistoricoAssociado;
+
+
+const HistoricoAssociado = () => {
+
+    return (
+
+        <div className="main">
+            <div className="topo">
+                <h2>Histórico do associado (nome)</h2>
+            </div>
+            <Parte2 />
+            <div className="parte3">
+                <div className="painelInfos">
+                    <h1>Infos de acordo com o filtro</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HistoricoAssociado;
