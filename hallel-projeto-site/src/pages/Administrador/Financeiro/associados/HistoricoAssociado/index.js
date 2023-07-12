331a5dc9ab7bce627c950f5e51982b8739f93db4
MIGUEL-ARCANJO <2021333513@ifam.edu.br>
2023-07-12 14:36:44 -0400

    tela de ver infos individuais

--- hallel-projeto-site/src/pages/Administrador/Financeiro/associados/HistoricoAssociado/index.js
+++ hallel-projeto-site/src/pages/Administrador/Financeiro/associados/HistoricoAssociado/index.js
@@ -1,13 +1,48 @@
 
+import "./styleHistoricoAssociado.css"
+
+const Parte2 = () =>{
+
+    const op = [ 
+        'Anual',
+        'Mensal',
+        'Bimestral',
+        'Trimestral',
+        'Semestral'
+    ];
+
+    return(
+        <div className="divTwo">
+            <div className="infosVizuHist">
+                <h3>Nome: teste</h3>
+                <h3>Cpf: teste</h3>
+                <h3>Idade: teste</h3>
+            </div>
+            <div className="filtrosVizuHist">
+                <select>
+                    {op.map((op)=>(
+                        <option key={op}>{op}</option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+}
+
 const HistoricoAssociado = () =>{
 
     return(
-
-        <div>
-
-            <h3>Historico Associado</h3>
-        
-        </div>
+        <div className="main">
+            <div className="topoTwo">
+                <h1>Historico do associado (nome)</h1>
+            </div>
+            <Parte2/>
+            <div className="parte3Vizu">
+                <div>
+                <h1>Informções de acordo com o filtro</h1>
+                </div>
+            </div>
+        </div>          
     );
 }
 
