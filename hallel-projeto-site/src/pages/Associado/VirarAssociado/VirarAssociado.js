1602fed72dd9c5d3ef73b77cb94d7e83ad546478
devthiagoramon <dev.thiagoramon@gmail.com>
2023-08-09 21:37:49 -0400

    Termino do front de associados

--- hallel-projeto-site/src/pages/Associado/VirarAssociado/VirarAssociado.js
+++ hallel-projeto-site/src/pages/Associado/VirarAssociado/VirarAssociado.js
@@ -3,18 +3,49 @@ import "./virar_associado.css";
 import VirarAssociadoP1 from "./VirarAssociadoP1";
 import { useState } from "react";
 import VirarAssociadoP2 from "./VirarAssociadoP2";
+import VirarAssociadoP3 from "./VirarAssociadoP3";
+import dayjs from "dayjs";
 
 const VirarAssociado = () => {
   const [indexParte, setIndexParte] = useState(0);
-  return <div className="container_principal_VA">
-    {indexParte === 0 &&
-        <VirarAssociadoP1 setIndexParte={setIndexParte}/>
-    }
-    {indexParte === 1 &&
-        <VirarAssociadoP2/>
-    }
-
-  </div>;
+  const associadoModel = {
+    nome: "",
+    email: "",
+    cpf: "",
+    telefone: "",
+    dataAniversario: dayjs(),
+    num_cartao: "",
+    data_validade_cartao: dayjs(),
+    cvc_cartao: 0,
+    nome_titular_cartao: "",
+    endereco_cartao: "",
+  };
+  const [novoAssociado, setNovoAssociado] = useState(associadoModel);
+  return (
+    <div className="container_principal_VA">
+      {indexParte === 0 && (
+        <VirarAssociadoP1
+          setIndexParte={setIndexParte}
+          novoAssociado={novoAssociado}
+          setNovoAssociado={setNovoAssociado}
+        />
+      )}
+      {indexParte === 1 && (
+        <VirarAssociadoP2
+          setIndexParte={setIndexParte}
+          novoAssociado={novoAssociado}
+          setNovoAssociado={setNovoAssociado}
+        />
+      )}
+      {indexParte === 2 && (
+        <VirarAssociadoP3
+          setIndexParte={setIndexParte}
+          novoAssociado={novoAssociado}
+          setNovoAssociado={setNovoAssociado}
+        />
+      )}
+    </div>
+  );
 };
 
 export default VirarAssociado;
--- hallel-projeto-site/src/pages/Associado/VirarAssociado/VirarAssociadoP2.js
+++ hallel-projeto-site/src/pages/Associado/VirarAssociado/VirarAssociadoP2.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { CreditCard } from "@mui/icons-material";
-import { Tooltip } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 
-const VirarAssociadoP2 = () => {
+const VirarAssociadoP2 = ({
+  setIndexParte,
+  novoAssociado,
+  setNovoAssociado,
+}) => {
   return (
     <motion.div
       animate={{ x: 0 }}
@@ -22,29 +26,91 @@ const VirarAssociadoP2 = () => {
       <div className="body_parts_VA">
         <div className="input_bodys_VA">
           <label>Número do Cartão</label>
-          <input id="telefone_associado" />
-        </div>
-        <div className="input_bodys_VA">
-          <label>Data de Validade</label>
-          <DatePicker
-            format="MM/YY"
-            sx={{width: "40%"}}
+          <input
+            id="num_cartao_associado"
+            type="text"
+            value={novoAssociado.num_cartao}
+            onChange={(e) => {
+              setNovoAssociado((prev) => {
+                return { ...prev, num_cartao: e.target.value };
+              });
+            }}
           />
         </div>
-        <div className="input_bodys_VA">
+        <div className="input_bodys2_VA">
+          <label>Data de Validade</label>
           <label>CVC</label>
+          <DatePicker
+            views={["year", "month"]}
+            id="data_validade_associado"
+            format="MM/YY"
+            sx={{ width: "60%", height: "50px", padding: 0 }}
+            value={novoAssociado.data_validade_cartao}
+            onChange={(e) => {
+              setNovoAssociado((prev) => {
+                return { ...prev, data_validade_cartao: e };
+              });
+            }}
+          />
           <Tooltip title="Código de Segurança">
-            <input id="telefone_associado" />
+            <input
+              id="cvc_associado"
+              type="number"
+              style={{ width: "90%" }}
+              value={novoAssociado.cvc_cartao}
+              onChange={(e) => {
+                setNovoAssociado((prev) => {
+                  return { ...prev, cvc_cartao: e.target.value };
+                });
+              }}
+            />
           </Tooltip>
         </div>
         <div className="input_bodys_VA">
-          <label>Telefone</label>
-          <input id="telefone_associado" />
+          <label>Nome do Titular</label>
+          <input
+            id="nome_titular_associado"
+            type="text"
+            value={novoAssociado.nome_titular_cartao}
+            onChange={(e) => {
+              setNovoAssociado((prev) => {
+                return { ...prev, nome_titular_cartao: e.target.value };
+              });
+            }}
+          />
         </div>
         <div className="input_bodys_VA">
-          <label>Telefone</label>
-          <input id="telefone_associado" />
+          <label>Endereço</label>
+          <input
+            id="endereco_cartao_associado"
+            type="text"
+            value={novoAssociado.endereco_cartao}
+            onChange={(e) => {
+              setNovoAssociado((prev) => {
+                return { ...prev, endereco_cartao: e.target.value };
+              });
+            }}
+          />
         </div>
+      </div>
+      <div className="footer_parts_VA">
+        <Button
+          onClick={() => {
+            setIndexParte(0);
+          }}
+          className="btn_virar_associado_voltar"
+          variant="contained"
+        >
+          Voltar
+        </Button>
+        <Button
+          onClick={() => {
+            setIndexParte(2);
+          }}
+          variant="contained"
+        >
+          Próximo
+        </Button>
       </div>
     </motion.div>
   );
