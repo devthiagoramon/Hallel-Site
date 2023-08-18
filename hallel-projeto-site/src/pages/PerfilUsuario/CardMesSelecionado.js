6c44db7b92570aa9902aed1915535a59bdd1fd7e
devthiagoramon <dev.thiagoramon@gmail.com>
2023-08-18 16:15:27 -0400

    commit extra para editar, ramon 18/08

--- hallel-projeto-site/src/pages/PerfilUsuario/CardMesSelecionado.js
+++ hallel-projeto-site/src/pages/PerfilUsuario/CardMesSelecionado.js
@@ -1,28 +1,54 @@
+import {
+  CheckCircleOutlineRounded,
+  CheckCircleRounded,
+} from "@mui/icons-material";
+import { Card, CardContent, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import React from "react";
 import { useMemo } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const CardMesSelecionado = ({ mesSelecionado, pagamentosAssociado }) => {
   const [pagamentoAssociadoSelecionado, setpagamentoAssociadoSelecionado] =
     useState(null);
 
-  useMemo(() => {
+  useEffect(() => {
     if (pagamentosAssociado !== undefined) {
       pagamentosAssociado.forEach((pagamento) => {
+        console.log(dayjs(pagamento.date).format("MM/YYYY"))
         if (
           dayjs(pagamento.date).format("MM/YYYY") ===
-          dayjs(mesSelecionado).format("MM/YYYY")
+          mesSelecionado
         ) {
+          console.log("Verdade")
           setpagamentoAssociadoSelecionado(pagamento);
         }
       });
     }
-  }, [mesSelecionado, setpagamentoAssociadoSelecionado, pagamentosAssociado]);
+  }, [mesSelecionado]);
   return (
     <div className="container_mesSelecionadoInfo">
-      {pagamentoAssociadoSelecionado !== null ? <div></div> : <div></div>}
+      {pagamentoAssociadoSelecionado !== null ? (
+        <Card variant="outlined" className="cont_card_pagamentoAssociado">
+          <CardContent>
+            <motion.div
+              initial={{ scale: 1.4 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 1 }}
+              className="headerpagamentoassociado pagamentoassociado_pago"
+            >
+              <CheckCircleOutlineRounded
+                sx={{ fontSize: "50px", color: "#F4F4FA" }}
+              />
+            </motion.div>
+            <Typography variant="label">Teste</Typography>
+          </CardContent>
+        </Card>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
