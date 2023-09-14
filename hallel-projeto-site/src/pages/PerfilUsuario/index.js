6249ddccfd3159e3c93f226bebf5755f915958af
devthiagoramon <dev.thiagoramon@gmail.com>
2023-09-13 23:26:34 -0400

    Começo alteração perfil associado

--- hallel-projeto-site/src/pages/PerfilUsuario/index.js
+++ hallel-projeto-site/src/pages/PerfilUsuario/index.js
@@ -10,6 +10,10 @@ import { associadoListarPerfil } from "../../api/uris/AssociadosURLS";
 import SelecionarMesesPagoAssociadoPerfil from "./SelecionarMesesPagoAssociadoPerfil";
 import dayjs from "dayjs";
 import CardMesSelecionado from "./CardMesSelecionado";
+import { IconButton, Tooltip, Typography } from "@mui/material";
+import { CalendarMonth } from "@mui/icons-material";
+import MenuCalendarioSelecionar from "../Administrador/Financeiro/associados/MenuCalendarioSelecionar";
+import { motion } from "framer-motion";
 
 const PerfilRow = () => {
   return (
@@ -21,6 +25,7 @@ const PerfilRow = () => {
 
 const Info = () => {
   const [usuario, setUsuario] = useState({});
+  const [anchorMenuCalendario, setAnchorMenuCalendario] = useState(null);
 
   const [passwordType, setPasswordType] = useState("password");
   const [passwordInput, setPasswordInput] = useState("");
@@ -29,6 +34,8 @@ const Info = () => {
   const [mesSelecionado, setMesSelecionado] = useState(
     dayjs().format("MM/YYYY")
   );
+
+  const [mesSelecionadoSorteio, setMesSelecionadoSorteio] = useState(dayjs());
 
   const handlePasswordChange = (evnt) => {
     setPasswordInput(evnt.target.value);
@@ -40,6 +47,10 @@ const Info = () => {
     }
     setPasswordType("password");
   };
+
+  function handleClickAbrirMenuCalendario(e) {
+    setAnchorMenuCalendario(e.currentTarget);
+  }
 
   useMemo(() => {
     let roles = String(localStorage.getItem("R0l3s"));
@@ -60,7 +71,6 @@ const Info = () => {
         },
       })
       .then((object) => {
-
         // Adiciona meses ao ultimo mês que o associado pagou
         if (roles.includes("ROLE_ASSOCIADO")) {
           let pagamentosAssociado = object.data.pagamentosAssociado;
@@ -71,8 +81,8 @@ const Info = () => {
           for (let index = 0; index < 3; index++) {
             let proximoMes = lastMes.add(1, "month");
             let objAux = {
-              dataPaga: proximoMes.toDate()
-            }
+              dataPaga: proximoMes.toDate(),
+            };
             pagamentosAssociado.push(objAux);
             lastMes = proximoMes;
           }
@@ -212,6 +222,40 @@ const Info = () => {
               mesSelecionado={mesSelecionado}
               pagamentosAssociado={usuario.pagamentosAssociado}
             />
+          </div>
+          <div className="sorteio_perfil_associado">
+            <div className="header_sort_perfil_assoc">
+              <h3>Sorteios</h3>
+              <div id="selecionar_mes_assoc_perfil_sorteios">
+                <div className="cont_mes_header_selecionado">
+                  <Typography variant="subtitle1">Mês selecionado</Typography>
+                  <label>{mesSelecionadoSorteio.format("MM/YYYY")}</label>
+                </div>
+                <Tooltip title="Selecionar o mês">
+                  <IconButton
+                    id="btn_abrir_calendario"
+                    onClick={(e) => {
+                      handleClickAbrirMenuCalendario(e);
+                    }}
+                  >
+                    <CalendarMonth
+                      sx={{ color: "blue", height: "35px", width: "35px" }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <MenuCalendarioSelecionar
+                anchorMenuCalendario={anchorMenuCalendario}
+                setAnchorMenuCalendario={setAnchorMenuCalendario}
+                mesSelecionado={mesSelecionadoSorteio}
+                setMesSelecionado={setMesSelecionadoSorteio}
+              />
+            </div>
+            <div className="body_sort_perfil_assoc">
+              <motion.div className="carousel">
+                  
+              </motion.div>
+            </div>
           </div>
         </div>
       )}
