269cb24ada4df5e016e8c35471f132b06839b241
fernandafreitas <fernandasouza0868@icloud.com>
2023-10-24 22:09:05 -0400

    cadastro

--- hallel-projeto-site/src/pages/SolicitarCadastro/index.js
+++ hallel-projeto-site/src/pages/SolicitarCadastro/index.js
@@ -1,114 +1,95 @@
-import React, {useState} from "react";
-import "./style.css";
-import Logo from "../../images/LogoHallel.png";
-import {Alert, Snackbar} from "@mui/material";
-import {solicitarCadastroService} from "../../service/HomeService";
+import './style.css';
+import React, { useState } from 'react';
+import Logo from './../../images/logoHallel2.png'
+import { IconButton, InputAdornment } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Link } from 'react-router-dom';
 
-const SolicitarCadastro = (props) => {
-    const [nomeInput, setNome] = useState();
-    const [emailInput, setEmail] = useState();
-    const [senhaInput, setSenha] = useState();
-    const [senha2Input, setSenha2] = useState();
+export default function Cadastro(){
+    const [showPassword, setShowPassword] = useState(false);
 
-    const [mostrarPopUp, setMostrarPopUp] = useState(false);
-    const [isValid, setisValid] = useState(false);
-    const [isValidError, setIsValidError] = useState(false);
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
+    const [rememberMe, setRememberMe] = useState(false);
 
-    function solicitar() {
-        if (senhaInput === senha2Input) {
-            let cadastro = {
-                nome: nomeInput,
-                email: emailInput,
-                senha: senhaInput
-            };
-            solicitarCadastroService(cadastro).then((response) => {
-                if (response) {
-                    setMostrarPopUp(true);
-                    setisValid(true);
-                    setTimeout(() => {
-                        window.location.href = "/";
-                    }, 3000);
-                } else {
-                    setIsValidError(true);
-                    setTimeout(() => {
-                        setMostrarPopUp(false);
-                    }, 3000);
-                }
-            });
-
-        }
-    }
-
-    const handleClose = () => {
-        setIsValidError(false);
-        setisValid(false);
+    const handleRememberMe = (event) => {
+        setRememberMe(event.target.checked);
     };
 
-    return (
-        <div>
-            <section className="containerGeralSC">
-                <section className="containerSecSC">
-                    <div className="headSC">
-                        <img id="ImgLogoSC" src={Logo} alt="logo site"></img>
-                        <div className="textoHeadSC">
-                            <h1 id="solicitarSC">SOLICITAR</h1>
-                            <h1 id="cadSC">CADASTRO</h1>
-                        </div>
+    return(
+         <div className='Cadastro-master'>
+            <div className='logo'>
+                <img src={Logo} className='img' alt='logo'/>
+            </div>
+            <div className='container-Cadastro'>
+                <div className='square'>
+                <div className='div-Cadastro'>
+                    <h1 className='Cadastro-text'>Cadastro</h1>
+                </div>
+                <div className='txt-input'>
+                    <div className='txt-text-div'>
+                        <span className='txt-text'>Nome completo:</span>
                     </div>
-                    <div className="informacoesSC">
-                        <input
-                            type="text"
-                            placeholder="Nome completo"
-                            value={nomeInput}
-                            onChange={(e) => setNome(e.target.value)}
-                        ></input>
-                        <input
-                            type="email"
-                            placeholder="Insira seu email"
-                            value={emailInput}
-                            onChange={(e) => setEmail(e.target.value)}
-                        ></input>
-                        <input
-                            type="password"
-                            placeholder="Crie uma senha"
-                            value={senhaInput}
-                            onChange={(e) => setSenha(e.target.value)}
-                        ></input>
-                        <input
-                            type="password"
-                            placeholder="Confirme sua senha"
-                            value={senha2Input}
-                            onChange={(e) => setSenha2(e.target.value)}
-                        ></input>
+                    <input type='text' placeholder='' className='txt-camp'  />
+                </div>
+
+                <div className='txt-input'>
+                    <div className='txt-text-div'>
+                        <span className='txt-text'>E-mail:</span>
                     </div>
-                    <div className="footerSC">
-                        <button id="JatemSC" onClick={solicitar}>
-                            ENVIAR
-                        </button>
-                        <div className="textoFooterSC">
-                            <a id="textoJatemSC">JA TEM UMA CONTA?</a>
-                            <a href="/entrar" id="entreSC">
-                                Entre aqui
-                            </a>
-                        </div>
+                    <input type='email' placeholder='' className='txt-camp'  />
+                </div>
+
+                <div className='txt-input'>
+                    <div className='txt-text-div'>
+                        <span className='txt-text' >Senha:</span>
                     </div>
-                </section>
-            </section>
-            <Snackbar
-                open={isValidError}
-                onClose={handleClose}
-                autoHideDuration={3000}
-            >
-                <Alert severity="error">Error na solicitação de cadastro</Alert>
-            </Snackbar>
-            <Snackbar open={isValid} onClose={handleClose} autoHideDuration={3000}>
-                <Alert severity="success">
-                    Solicitado com sucesso, aguarde a nossa equipe administrativa aceitar
-                    a sua solicitação.
-                </Alert>
-            </Snackbar>
+                    <input type='password' placeholder='' className='txt-camp'/>
+                   
+                </div>
+
+
+
+                <div className='txt-input'>
+                    <div className='txt-text-div'>
+                        <span className='txt-text' >Confirmar senha:</span>
+                    </div>
+                    <input type={showPassword ? 'text' : 'password'} placeholder='' className='txt-camp'/>
+                    <InputAdornment>
+                        <IconButton onClick={handleTogglePassword} className='button-password' style={{ marginLeft: '440px', marginTop: '-40px', color: 'white'}}>
+                            {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </IconButton>
+                    </InputAdornment>
+                </div>
+                   
+
+                   
+
+               <div>
+                    <button className='button-continueC'>Continuar</button>
+                </div>  
+
+                <div className='register-divC'>
+                    <p className='text-registerC'>Já tem uma conta?
+                    <Link to="/entrar" className='registerC'>Conecte-se</Link>
+                    </p>
+                </div> 
+
+                <div className='terms-divC'>
+                    <p className='termsC'>Ao continuar, você aceita nossos
+                    <br/>
+                    <a href="#" className='register-termsC'> Termos de condições </a> e 
+                    
+                    <a href="#" className='register-privaceC'> Política de privacidade</a>
+                    </p>
+                </div>   
+
+   
+
+            </div>
+            </div>
         </div>
     );
-};
-
-export default SolicitarCadastro;
+}
