76b0fb6c6c28e0f7e0db2098053ef5054d55555d
fernandafreitas <fernandasouza0868@icloud.com>
2023-11-10 15:02:32 -0400

    slide prog faltando ajustes

--- hallel-projeto-site/src/components/SlideProg/index.js
+++ hallel-projeto-site/src/components/SlideProg/index.js
@@ -8,70 +8,151 @@ import Img5 from "../../images/Sexta.png";
 import Img6 from "../../images/Sabado.png";
 import Img7 from "../../images/Domingo.png";
 import "./styleProg.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const SlideProg = (props) => {
-  const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7];
-
-  const carrosel = useRef();
-  const [width, setWidth] = useState(0);
-  const [currentImage, setCurrentImage] = useState(0);
-
-  useEffect(() => {
-    setWidth(carrosel.current?.offsetWidth);
-
-    const interval = setInterval(() => {
-      setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [images.length]);
-
-  const handleDotClick = (index) => {
-    setCurrentImage(index);
+  const configuracoes = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   };
 
+  const handleMouseEnter = (event) => {
+    event.currentTarget.style.transform = 'scale(0.9)';
+  }
+
+  const handleMouseLeave = (event) => {
+    event.currentTarget.style.transform = 'scale(0.8)';
+  }
+
   return (
-    <div className="body-programacao">
-      <h1 id="title">Programação Semanal</h1>
-      <div className="containerCarroseulProg">
-        <div className="carrousel" ref={carrosel}>
-          <motion.div
-            className="inner"
-            style={{
-              display: 'flex',
-              transition: 'transform 0.5s ease-in-out',
-              transform: `translateX(${-currentImage * (100 / images.length)}%)`
-            }}
-          >
-            {images?.map((imagem2, index) => {
-              return (
-                <motion.div
-                  className="imagem2"
-                  key={index}
-                  whileHover={{ scale: "1.02" }}
-                  style={{ flex: `0 0 ${100 / images.length}%` }}
-                >
-                  <img src={imagem2} alt="img" />
-                  <div className="legenda2">
-                    <a href="">Saiba mais</a>
-                  </div>
-                </motion.div>
-              );
-            })}
-          </motion.div>
-        </div>
-        <div className="dots">
-          {images.map((_, index) => (
-            <div
-              key={index}
-              className={`dot ${index === currentImage ? 'active' : ''}`}
-              onClick={() => handleDotClick(index)}
-            ></div>
-          ))}
+
+    <div className="destaque">
+      <h3 style={{textAlign:'center',fontSize:'40px', color:'#003015'}}>Programação Semanal</h3>
+      <Slider {...configuracoes}>
+      
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img1} alt="Produto 1" />
+          <div className="texto-promocao">
+            <span className="nome-produto">Segunda</span>
+            <hr className="linha-separadora" /> 
+            <div className="promocao">
+              
+              <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
         </div>
       </div>
+      
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img2} alt="Produto 2" />
+          
+          <div className="texto-promocao">
+            <span className="nome-produto">Terça</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+              
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img3} alt="Produto 3" />
+          
+          <div className="texto-promocao">
+            <span className="nome-produto">Quarta</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+              
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img4} alt="Produto 4" />
+         
+          <div className="texto-promocao">
+            <span className="nome-produto">Quinta</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+              
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img5} alt="Produto 5" />
+          
+          <div className="texto-promocao">
+            <span className="nome-produto">Sexta</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+             
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img6} alt="Produto 5" />
+          
+          <div className="texto-promocao">
+            <span className="nome-produto">Sábado</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+             
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="produto" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="conteudo-produto">
+          <img src={Img7} alt="Produto 6" />
+          
+          <div className="texto-promocao">
+            <span className="nome-produto">Domingo</span>
+            <hr className="linha-separadora" />
+            <div className="promocao">
+              
+            <a href="#" className="valorPromocao">Saiba mais</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Slider>
     </div>
-  );  
+
+    
+
+  );
 };
 
 export default SlideProg;
