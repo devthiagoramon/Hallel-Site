713751da1866e939e266b402f5b070a3ec63b48d
fernandafreitas <fernandasouza0868@icloud.com>
2023-11-17 17:32:01 -0400

    cards home

--- hallel-projeto-site/src/components/SejaAssociadoHome/associado.js
+++ hallel-projeto-site/src/components/SejaAssociadoHome/associado.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../../components/SejaAssociadoHome/associado.css";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 function SejaAssociado() {
   const [expanded, setExpanded] = useState(false);
@@ -13,20 +14,28 @@ function SejaAssociado() {
       <div className="container-boxes">
         <div className={`container-doacao ${expanded ? "expanded" : ""}`} onClick={toggleExpansion}>
           <span className="text-main">Faça uma doação</span>
+          {!expanded && <IoIosArrowDown className="arrow"/>}
           {expanded && (
             <div className="additional-content">
               <p className="text-p">Faça a diferença hoje: Sua doação à Comunidade Hallel transforma vidas e espalha alegria!</p>
-              <a href="#">Saiba mais</a>
+              <div  className="div-link">
+              <a className="link" href="/doacoes">Saiba mais</a>
+              </div>
+              <IoIosArrowUp className="arrow-up"/>
             </div>
           )}
         </div>
 
         <div className={`container-doacao ${expanded ? "expanded" : ""}`} onClick={toggleExpansion}>
           <span className="text-main">Seja um Associado Hallel</span>
+          {!expanded && <IoIosArrowDown className="arrow"/>}
           {expanded && (
             <div className="additional-content">
               <p className="text-p">Seja um Associado Hallel e conheça nossos eventos! Descubra como sua ajuda é fundamental.</p>
-              <a href="#">Saiba mais</a>
+              <div  className="div-link">
+              <a className="link" href="#">Saiba mais</a>
+              </div>
+              <IoIosArrowUp className="arrow-up"/>
             </div>
           )}
         </div>
