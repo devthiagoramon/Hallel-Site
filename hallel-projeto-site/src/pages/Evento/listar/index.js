36e232d5d67032c36d0c39ddb039edb77945cd89
fernandafreitas <fernandasouza0868@icloud.com>
2024-01-10 17:06:54 -0400

    tela de ver eventos finalizada

--- hallel-projeto-site/src/pages/Evento/listar/index.js
+++ hallel-projeto-site/src/pages/Evento/listar/index.js
@@ -6,6 +6,7 @@ import "./eventoUser.css";
 import dayjs from "dayjs";
 import SearchIcon from "@mui/icons-material/Search";
 import { listarTodosEventosUsuarioService } from "../../../service/EventoService";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 function EventoUser() {
   const [showInfos, setshowInfos] = useState(false);
@@ -51,32 +52,32 @@ function EventoUser() {
         />
       ) : (
         <div className="master">
-          <div className="top"> 
-          <div className="title" style={{margin:'0 auto'}}>
-          <h1 className="TituloEventoUser">Eventos</h1>
-          </div>
-          <div className="contPesquisaEventoUser">
-          <TextField
-            id="pesquisaEvents"
-            type="text"
-            variant="outlined"
-            size="small"
-            label="Pesquisa..."
-            onChange={filterEventos}
-            
-              sx={{
+          <div className="top">
+            <div className="title" style={{ margin: '0 auto' }}>
+              <h1 className="TituloEventoUser">Eventos</h1>
+            </div>
+            <div className="contPesquisaEventoUser">
+              <TextField
+                id="pesquisaEvents"
+                type="text"
+                variant="outlined"
+                size="small"
+                label="Pesquisa..."
+                onChange={filterEventos}
+
+                sx={{
                   borderRadius: "0px",
                   width: "300px",
                   backgroundColor: '#E6EEE9',
                   border: 'none',
-                    }}
-                    InputProps={{
-                      endAdornment: (
-                        <SearchIcon style={{ color: "#003617" }} />
-                      ),
-                    }}
-          />
-          </div>
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <SearchIcon style={{ color: "#003617" }} />
+                  ),
+                }}
+              />
+            </div>
           </div>
 
           <div className="CorpoEventoUser">
@@ -92,49 +93,13 @@ function EventoUser() {
                 <Skeleton width={250} height={500} />
               </div>
             ) : (
-              eventos?.map((evento) => {
-                return (
-                  <div className="grupo-evento">
-                    <div
-                      className="card"
-                      style={{ width: "17rem", maxHeight: "480px",borderRadius:0,
-                      borderBottomLeftRadius: "20px",
-                      borderBottomRightRadius: "20px",}}
-                    >
-                      <img
-                        src={evento.imagem}
-                        className="card-img-top"
-                        alt="..."
-                        style={{ maxHeight: "200px", 
-                        minHeight: "200px",
-                       }}
-                      />
-                      <hr style={{marginTop:'0px'}}/>
-                      <div className="card-body">
-                        <h5 className="card-title" style={
-                          {
-                            color:'#00471F',
-                            fontSize: '25px',
-                            marginTop:'-10px'
-                          }
-                        }>{evento.titulo}</h5>
-                        <p className="card-text">{evento.descricao}</p>
-                        <br />
-                        <p className="card-text">
-                          Data: {dayjs(evento.date).format("DD/MM/YYYY")}
-                        </p>
-                        <br />
-                        <button
-                          className="btn btn-primary"
-                          onClick={() => alterarEventoEspc(evento)}
-                        >
-                          Saber mais
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })
+              eventos?.map((evento) => (
+                <EventoCard
+              key={evento.id}
+              evento={evento}
+              alterarEventoEspc={alterarEventoEspc}
+            />
+              ))
             )}
           </div>
         </div>
@@ -142,5 +107,94 @@ function EventoUser() {
     </div>
   );
 }
+
+const EventoCard = ({ evento, alterarEventoEspc }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div className={`grupo-evento ${expanded ? "expanded" : ""}`} key={evento.id}>
+      <div
+        className="card"
+        style={{
+          width: "17rem",
+          height: expanded ? "460px" : "400px",
+          marginBottom: '2rem',
+          borderRadius: 0,
+          borderBottomLeftRadius: "20px",
+          borderBottomRightRadius: "20px",
+          overflow: 'hidden',
+          position: 'relative',
+          marginTop: "-4rem"
+        }}
+      >
+        <img
+          src={evento.imagem}
+          className="card-img-top"
+          alt="..."
+          style={{
+            maxHeight: "200px",
+            minHeight: "200px",
+          }}
+        />
+        <hr style={{ marginTop: '0px' }} />
+        <div className="card-body">
+          <h5 className="card-title" style={
+            {
+              color: '#00471F',
+              fontSize: '25px',
+              marginTop: '-1rem'
+            }
+          }>{evento.titulo}</h5>
+          <p className="card-text" style={{
+            color: '#00471F',
+            display: '-webkit-box',
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+            WebkitLineClamp: 2,
+          }}>{evento.descricao}</p>
+
+          <div className="dateLine" style={{
+            width: expanded ? '100%' : '100%',
+            display: 'flex',
+            alignItems: 'center',
+            position: 'absolute',
+            bottom: '0',
+            marginLeft: expanded ? '-1rem' : '-1rem',
+            borderRadius: expanded ? '12px' : '12px',
+          }}>
+            <p className="card-text" style={{
+              position: 'absolute',
+              bottom: '0',
+              marginLeft: '0.5rem',
+              marginBottom: '0.5rem',
+              fontWeight:'600',
+              color: '#00471F',
+            }}>
+              Data: {dayjs(evento.date).format("DD/MM/YYYY")}
+            </p>
+            
+            <button
+              className="expand-button"
+              onClick={handleExpand}
+              style={{ marginLeft: 'auto',marginRight:'0.9rem',backgroundColor:'#003617', borderRadius:'100px',width:'22px',height:'20px',alignItems:'center',display:'flex'}}
+            >
+              {expanded ? <IoIosArrowUp style={{color:'white',margin:'0 auto'}}/> : <IoIosArrowDown style={{color:'white',margin:'0 auto'}}/>}
+            </button>
+          </div>
+          {expanded && (
+            <button className="saber-mais-button" onClick={() => alterarEventoEspc(evento)}>
+              Saber mais
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export default EventoUser;
