c498d149a072d8af2882164f28e3283b85015c5f
FELIPE-GSV <felipefeltz232@gmail.com>
2024-07-21 12:50:12 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-          {/* <Route  path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} /> */}
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Route path="/" element={<Home />} />
+    </BrowserRouter>
   );
 }
 
