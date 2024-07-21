60714719d2eb1bee806124efe2967167ee2d4afa
devthiagoramon <dev.thiagoramon@gmail.com>
2024-07-21 12:34:27 -0400

    Config Dirs

---
+++ src/route.tsx
@@ -0,0 +1,12 @@
+
+const Router = () => {
+  return (
+    <Router>
+          {/* <Route  path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} /> */}
+    </Router>
+  )
+}
+
+export default Router
