4b84a1a5e97cba990bf9c937908a7b120748007a
FELIPE-GSV <felipefeltz232@gmail.com>
2024-07-21 12:26:37 -0300

    rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+          {/* <Route  path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} /> */}
+      </Router>
     </div>
   );
 }
